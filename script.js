af0096c1776105fd6937162ce99c13106ab8e31c
kombajner <matej.rupnik02@gmail.com>
2021-07-26 12:48:18 +0200

    koncan

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-for (let i = 0; i < ((16*16)); i++) {
-    let div = document.createElement(`div`);
-    div.className = `div`;
-    div.textContent = `test`;
+let x = 16;
+let y = 16;
 
+function generate(){
+for (let i = 0; i < ((x*y)); i++) {
+    var div = document.createElement(`div`);
+    div.className = `div`;
+    div.textContent = ``;
+    div.addEventListener(`mouseenter`, function(e) {
+    e.target.classList.add(`hover`);
+});
     document.getElementById(`container`).appendChild(div);
-}
+};
+document.getElementById(`container`).style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+};
+generate();
+
+
+let allDivs = Array.from(document.querySelectorAll(`.div`));
+let btn = document.getElementById(`reset`);
+btn.addEventListener(`click`, function(){
+allDivs.forEach(element => {
+    element.classList.remove(`hover`);
+});
+do {
+x = window.prompt(`test`, ``);
+} while (x > 100);
+
+do {
+y = window.prompt(`test`, ``);
+}while (y > 100);
+
+let container = document.getElementById(`container`);
+let div = document.getElementsByClassName(`div`);
+let arrDiv = Array.from(div);
+console.log(arrDiv);
+arrDiv.forEach(element =>
+    container.removeChild(element)
+);
+generate();
+
+});
