9ec87f1d6dbac55513bea3561446298bd24848d4
Matej Rupnik <matejrupnik@pop-os.localdomain>
2021-07-26 09:48:07 +0200

    inital commit

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < ((16*16)); i++) {
+    let div = document.createElement(`div`);
+    div.className = `div`;
+    div.textContent = `test`;
+
+    document.getElementById(`container`).appendChild(div);
+}
